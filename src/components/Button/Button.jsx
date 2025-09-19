f1478a290731904d77db49f7b4437d62b85cf8ca
rodnulka-3 <leninskaya06@bk.ru>
2025-09-19 20:26:26 +0300

    add layout

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,11 @@
-function Button() {
-  return <button>Сохранить</button>;
+import './Button.css';
+
+function Button({ text, onClick }) {
+  return (
+    <button className="button accent" onClick={onClick}>
+      {text}
+    </button>
+  );
 }
 
 export default Button;
